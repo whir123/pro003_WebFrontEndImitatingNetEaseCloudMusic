9a108a9cb75e49376eafd95127b4ccd7a7d86390
whir123 <1549820081@qq.com>
2025-11-29 12:54:23 +0800

    251129 路由懒加载组件分包

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { useRoutes } from 'react-router-dom'
+import React, { Suspense } from 'react';
+import { useRoutes, Link } from 'react-router-dom'
 import routes from './router';
-import Download from './views/download';
+// import Download from './views/download';
 
 function App() {
-  const discover = useRoutes(routes);
   return (
     <div className='App'>
-      HELLO(测试页面用)
-      {/* <Router path='/路径' element={组件}/> */}
-      {/* <Router path='/路径' element={组件}/> */}
-      {/* <Router path='/路径' element={组件}/> */}
-      {/* <Router path='/路径' element={组件}/> */}
-      {/* 路由统一配置到配置文件： router/index.tsx */}
-      {discover}
+      {/* HELLO(测试页面用) */}
+      <div className='nav'>
+        <Link to="/discover">发现音乐</Link>
+        <Link to="/mine">我的音乐</Link>
+        <Link to="/focus">关注</Link>
+        <Link to="/download">下载客户端</Link>
+      </div>
+      <Suspense fallback="loading...">
+      {/* 让 React 在加载期间显示一个“过渡 UI（loading）”，避免白屏 */}
+        <div className='main'>{useRoutes(routes)}</div>
+        {/* useRoutes() 是 React Router v6 提供的一个核心 Hook，
+        用来根据传入的“路由配置表”动态生成 <Route /> 结构，并返回对应的要渲染的 UI 组件 */}
+      </Suspense>
     </div>
   )
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { lazy } from "react";
 import { Navigate } from "react-router-dom"; // ⚠️ Navigate 是一个 React 组件 把用户自动重定向到指定路径
 import type { RouteObject } from "react-router-dom";
 
-import Discover from "@/views/discover";
-import Mine from "@/views/mine";
-import Focus from "@/views/focus";
-import Download from "@/views/download";
+// ⬇️ 这些页面组件会在浏览器第一次加载时全部被打包进主bundle.js，即使用户只访问/discover，也会把其他几个大页面一起加载
+// import Discover from "@/views/discover";
+// import Mine from "@/views/mine";
+// import Focus from "@/views/focus";
+// import Download from "@/views/download";
+
+// ⚠️ React.lazy() 的核心作用：让路由组件按需加载（代码分割），从而大幅减少首屏体积，提高页面加载速度
+// ⚠️ 动态 import，打包器（Vite / Webpack）会自动把每个页面单独拆成一个 chunk
+const Discover = lazy(() => import("@/views/discover"));
+const Mine = lazy(() => import("@/views/mine"));
+const Focus = lazy(() => import("@/views/focus"));
+const Download = lazy(() => import("@/views/download"));
 
 const routes:RouteObject[] = [
     {
